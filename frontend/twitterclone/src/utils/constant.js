379f1c273efb4aa7b8787a91357748af9626daa2
Abhishek <abhishekrana4a0@gmail.com>
2024-06-10 12:37:49 +0530

    cors realted changes

--- frontend/twitterclone/src/utils/constant.js
+++ frontend/twitterclone/src/utils/constant.js
@@ -1,8 +1,13 @@
 // export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
 // export const TWEET_API_END_POINT = "http://localhost:8080/api/v1/tweet";
 
-export const USER_API_END_POINT = "https://new-twitter-clone-bc.vercel.app/api/v1/user";
-export const TWEET_API_END_POINT = "https://new-twitter-clone-bc.vercel.app/api/v1/tweet";
+// export const USER_API_END_POINT = "https://new-twitter-clone-bc.vercel.app/api/v1/user";
+// export const TWEET_API_END_POINT = "https://new-twitter-clone-bc.vercel.app/api/v1/tweet";
+
+
+export const USER_API_END_POINT = "https://new-twitter-clone.onrender.com/api/v1/user";
+export const TWEET_API_END_POINT = "https://new-twitter-clone.onrender.com/api/v1/tweet";
+
 
 export const timeSince = (timestamp) => {
     let time = Date.parse(timestamp);
