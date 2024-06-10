504d85bcc79dbce8a849a420fedb417db652c670
Abhishek <abhishekrana4a0@gmail.com>
2024-06-10 14:35:12 +0530

    change in indx.js

--- backend/index.js
+++ backend/index.js
@@ -63,22 +63,3 @@ app.listen(port,()=>{
     console.log(`Server is listing at port ${port}`);
 })
 
-// Export the app wrapped with allowCors for Vercel
-const allowCors = fn => async (req, res) => {
-  res.setHeader( "Access-Control-Allow-Origin", "https://new-twitter-clone-fe.vercel.app/" );
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // res.setHeader('Access-Control-Allow-Origin', '*'); // Or specify your domain
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-}
-
-module.exports = allowCors(app);
